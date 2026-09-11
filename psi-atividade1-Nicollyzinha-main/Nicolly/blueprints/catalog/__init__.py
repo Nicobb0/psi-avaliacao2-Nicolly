33983d6a3efb33a3e59d78a359afc8c1c33368af
Nicobb0 <dantas.nicolly@escolar.ifrn.edu.br>
2026-09-11 14:29:01 -0300

    Delete psi-atividade1-Nicollyzinha-main/Nicolly directory

--- psi-atividade1-Nicollyzinha-main/Nicolly/blueprints/catalog/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-catalog_bp = Blueprint("catalog", __name__, template_folder="templates")
-
-from blueprints.catalog import routes
--- psi-atividade1-Nicollyzinha-main/Nicolly/blueprints/servicos/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-servicos_bp = Blueprint("servicos", __name__, template_folder="templates")
-
-from blueprints.servicos import routes
